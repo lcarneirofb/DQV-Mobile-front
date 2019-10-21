d0de3d0d34b389fb97b3dd9348913b6679267b6b
Aldan <aldantavares@gmail.com>
2019-10-21 18:09:02 -0300

    Restarting the project cause of gradle issues

---
+++ app/src/main/java/com/example/dqv/MenuActivity.kt
@@ -0,0 +1,15 @@
+package com.example.dqv_front
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+    }
+
+}
