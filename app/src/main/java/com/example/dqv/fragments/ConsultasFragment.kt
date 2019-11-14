6f7309785c1cb4123b79c756a133cd0487692e6c
Lucas Carneiro <lcarneirofb@gmail.com>
2019-11-13 23:21:20 -0300

    Consultas Tab partially working, new icons

--- app/src/main/java/com/example/dqv/fragments/ConsultasFragment.kt
+++ app/src/main/java/com/example/dqv/fragments/ConsultasFragment.kt
@@ -7,7 +7,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import com.example.dqv.API.RetrofitInitializer
 import com.example.dqv.R
+import com.example.dqv.beans.AgendamentoConsulta
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_consultas.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,7 +44,7 @@ class ConsultasFragment : Fragment() {
             param2 = it.getString(ARG_PARAM2)
         }
 
-        //getData()
+        getData()
     }
 
     override fun onCreateView(
@@ -79,27 +88,46 @@ class ConsultasFragment : Fragment() {
         fun onFragmentInteraction(uri: Uri)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ConsultasFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ConsultasFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 
     fun getData(){
+        val callAgendamento = RetrofitInitializer().agendamentoConsultaService().getAgendamentosPessoa(1)
+        callAgendamento.enqueue(object: Callback<List<AgendamentoConsulta>?> {
+            override fun onResponse(call: Call<List<AgendamentoConsulta>?>,
+                                    response: Response<List<AgendamentoConsulta>?>) {
+                response?.body()?.let {
+                    val agendamentos: List<AgendamentoConsulta> = it
+                            for(i in agendamentos) {
+                                txt_especialidade.text = i.consulta?.especialista?.especialidade?.name
+                                txt_nome_medico.text = "Atendente:" + i.consulta?.especialista?.nome
+                                if(i.consulta?.status == true){
+                                    txt_status_consulta.text = "Status: Confirmada"
+                                }
+                                txt_data.text = i.consulta?.horario?.horaInico.toString()
+
+                                if(txt_especialidade.text == "ASSISTENTE_SOCIAL"){
+                                    img_consultTypeIcon.setImageResource(R.drawable.img_socialas)
+                                }else if(txt_especialidade.text == "PSICOLOGO"){
+                                    img_consultTypeIcon.setImageResource(R.drawable.img_psicologo)
+                                }else if(txt_especialidade.text == "NUTRICIONISTA"){
+                                    img_consultTypeIcon.setImageResource(R.drawable.img_nutricionista)
+                                }else if(txt_especialidade.text == "ORTODONTISTA"){
+                                    img_consultTypeIcon.setImageResource(R.drawable.img_ortodontista)
+                                }else if(txt_especialidade.text == "MEDICO"){
+                                    img_consultTypeIcon.setImageResource(R.drawable.img_medico)
+                                }
+                            }
+                }
+            }
+
+            override fun onFailure(call: Call<List<AgendamentoConsulta>?>,
+                                   t: Throwable?) {
+
+                Toast.makeText(context, t?.message, Toast.LENGTH_SHORT).show()
+                println(t?.message)
+                //Toast.makeText(applicationContext, "Algo de errado não está certo, estudantes sedentos "+
+                //        "estão trabalhando para resolver o seu problema, NTI PLS HALP", Toast.LENGTH_SHORT).show()
+            }
+        })
 
     }
 }
