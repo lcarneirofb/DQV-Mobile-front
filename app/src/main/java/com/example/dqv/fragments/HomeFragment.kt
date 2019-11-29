beb553db6e319abbc9eb16c654af55cd765640d5
Caio Souza <caiobrunos@hotmail.com>
2019-11-28 21:34:08 -0300

    New fragments, and image assets

--- app/src/main/java/com/example/dqv/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/dqv/fragments/HomeFragment.kt
@@ -7,7 +7,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.dqv.API.RetrofitInitializer
 import com.example.dqv.R
+import com.example.dqv.beans.AgendamentoConsulta
+import kotlinx.android.synthetic.main.fragment_consultas.*
+import kotlinx.android.synthetic.main.fragment_home.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,6 +42,8 @@ class HomeFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+        getDataAgendamentosHome()
     }
 
     override fun onCreateView(
@@ -95,5 +105,48 @@ class HomeFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    fun getDataAgendamentosHome(){
+        val callAgendamento = RetrofitInitializer().agendamentoConsultaService().getAgendamentosPessoa(1)
+        callAgendamento.enqueue(object: Callback<List<AgendamentoConsulta>?> {
+            override fun onResponse(call: Call<List<AgendamentoConsulta>?>,
+                                    response: Response<List<AgendamentoConsulta>?>
+            ) {
+                response?.body()?.let {
+                    val agendamentos: List<AgendamentoConsulta> = it
+                    for(i in agendamentos) {
+                        txt_especialidade_home.text = i.consulta?.especialista?.especialidade?.name
+                        txt_nome_medico_home.text = "Atendente:" + i.consulta?.especialista?.nome
+                        if(i.consulta?.status == true){
+                            txt_status_consulta_home.text = "Status: Confirmada"
+                        }
+                        txt_data_home.text = i.consulta?.horario?.horaInico.toString()
+
+                        if(txt_especialidade_home.text == "ASSISTENTE_SOCIAL"){
+                            img_consultTypeIcon_home.setImageResource(R.drawable.img_socialas)
+                        }else if(txt_especialidade_home.text == "PSICOLOGO"){
+                            img_consultTypeIcon_home.setImageResource(R.drawable.img_psicologo)
+                        }else if(txt_especialidade_home.text == "NUTRICIONISTA"){
+                            img_consultTypeIcon_home.setImageResource(R.drawable.img_nutricionista)
+                        }else if(txt_especialidade_home.text == "ORTODONTISTA"){
+                            img_consultTypeIcon_home.setImageResource(R.drawable.img_ortodontista)
+                        }else if(txt_especialidade_home.text == "MEDICO"){
+                            img_consultTypeIcon_home.setImageResource(R.drawable.img_medico)
+                        }
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<List<AgendamentoConsulta>?>,
+                                   t: Throwable?) {
+
+                Toast.makeText(context, t?.message, Toast.LENGTH_SHORT).show()
+                println(t?.message)
+                //Toast.makeText(applicationContext, "Algo de errado não está certo, estudantes sedentos "+
+                //        "estão trabalhando para resolver o seu problema, NTI PLS HALP", Toast.LENGTH_SHORT).show()
+            }
+        })
+
     }
 }
